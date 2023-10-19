d424dd0d5ff9f70b317dda0e6a718147e86f3622
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2023-10-20 02:28:06 +0900

    update context provider

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -1,20 +1,28 @@
 "use client";
-
 import { createContext, useContext, useEffect, useState } from "react";
 
 //context for preview data
 const PreviewContext = createContext({});
 
 export const PreviewContextProvider = ({ children }) => {
-  // Load data from localStorage on initial render
+  // Check if localStorage is available
+  const isLocalStorageAvailable = typeof localStorage !== "undefined";
+
+  // Load data from localStorage on initial render (client-side)
   const [preview, setPreview] = useState(() => {
-    const storedPreview = localStorage.getItem("preview");
-    return storedPreview ? JSON.parse(storedPreview) : {};
+    if (isLocalStorageAvailable) {
+      const storedPreview = localStorage.getItem("preview");
+      return storedPreview ? JSON.parse(storedPreview) : {};
+    } else {
+      return {};
+    }
   });
 
-  // Update localStorage whenever the preview state changes
+  // Update localStorage whenever the preview state changes (client-side)
   useEffect(() => {
-    localStorage.setItem("preview", JSON.stringify(preview));
+    if (isLocalStorageAvailable) {
+      localStorage.setItem("preview", JSON.stringify(preview));
+    }
   }, [preview]);
 
   return (
@@ -25,20 +33,21 @@ export const PreviewContextProvider = ({ children }) => {
 };
 
 export const usePreviewContext = () => useContext(PreviewContext);
-//-------------
 
 // context to get all diary data
 const DiaryContext = createContext({});
 
 export const DiaryContextProvider = ({ children }) => {
-  // Load data from localStorage on initial render
+  // Load data from an API on initial render (client-side)
   const [diaries, setDiaries] = useState();
   useEffect(() => {
-    fetch("../api/diary-data")
-      .then((res) => res.json())
-      .then((data) => {
-        setDiaries(data);
-      });
+    if (typeof window !== "undefined") {
+      fetch("../api/diary-data")
+        .then((res) => res.json())
+        .then((data) => {
+          setDiaries(data);
+        });
+    }
   }, []);
 
   return (
